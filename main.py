34143f94b87a88cdb64a38212dcce567ae15c1b4
sadness_ojisan <sadness.ojisan@gmail.com>
2019-05-04 19:08:33 +0900

    update: driver

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
+#coding:utf-8
+
 import time
 from selenium import webdriver
 import chromedriver_binary
+import json
 
+LOCAL_HOST = 'http://localhost:8080'
+
+f = open('./log.json', 'r')
+jsonData = json.load(f)
+f.close()
+logArray = jsonData['log']
 driver = webdriver.Chrome()
-driver.get('https://www.google.com/')
-time.sleep(5)
-search_box = driver.find_element_by_name("q")
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5)
-driver.quit()
+for log in logArray:
+    if log['eventType'] == 'LOAD': 
+        if driver.current_url != LOCAL_HOST + log['url']: 
+            driver.get(LOCAL_HOST + log['url'])
+    elif log['eventType'] == 'CLICK': 
+        if driver.find_element_by_xpath("//*[@data-testid='"+log['target']+"']"): 
+            target = driver.find_element_by_xpath("//*[@data-testid='"+log['target']+"']")
+            target.click()
+        else: 
+            driver.get(LOCAL_HOST + log['url'])
+    elif log['eventType'] == 'BLUR':
+        if driver.find_element_by_xpath("//*[@data-testid='"+log['target']+"']"): 
+            inputValue = log['property']['inputValue']
+            target = driver.find_element_by_xpath("//*[@data-testid='"+log['target']+"']")
+            target.send_keys(inputValue)
+        else: 
+            driver.get(LOCAL_HOST + log['url'])
+    elif log['eventType'] == 'SUBMIT': 
+        if driver.find_element_by_xpath("//*[@data-testid='"+log['target']+"']"): 
+            target = driver.find_element_by_xpath("//*[@data-testid='"+log['target']+"']")
+            target.click()
+        else: 
+            driver.get(LOCAL_HOST + log['url'])
+    else: 
+        driver.get(LOCAL_HOST + log['url'])
+    time.sleep(1)
